d36e74937a1e2af5a1099ef42e19d82f4db82735
unknown <nikos_nick@outlook.com>
2018-03-07 12:51:10 +0200

    Add new methods

--- monty_hall.py
+++ monty_hall.py
@@ -8,6 +8,8 @@ class MontyHall:
             N is the number of iterations needed to run the simulation
         """ 
         self.doors = ["Car", "Goat", "Goat"]
+        self.choice_of_host = None
+        self.new_choice_of_player = None
         self.num_of_wins = 0
         self.num_of_doors = len(self.doors)
         self.N = N
@@ -20,35 +22,48 @@ class MontyHall:
             i = self.pick_a_random_number(0,2)
             j = self.pick_a_random_number(0,2)
             self.doors[i], self.doors[j] = self.doors[j], self.doors[i]
-        
-        
-    def start_simulation(self):
-        """ Repeat the game N times and track the number of wins
-            when someone follow the strategy of switching doors
+    
+
+    def start_game(self):
+        """ Start a single game    
+            Player chooses a door randomly 
+            Host chooses a door, which has not been chosen by the player and has not the car prize
+            Player chooses to switch the door 
         """
         doors = self.doors
         
+        choice_of_player = self.pick_a_random_number(0,2)
+        
+        i = (choice_of_player - 1) % self.num_of_doors
+        j = (choice_of_player + 1) % self.num_of_doors
+        
+        if doors[choice_of_player] != "Car":
+            self.choice_of_host = i if doors[j] == "Car" else j
+        else:
+            self.choice_of_host = i if self.pick_a_random_number(0,1) <= 0.5 else j
+                   
+        self.new_choice_of_player = j if self.choice_of_host == i else i 
+         
+    
+    def player_wins(self):
+        """ Check if player wins
+        """
+        return self.doors[self.new_choice_of_player] == "Car"
+        
+    
+    def start_simulation(self):
+        """ Reset and repeat the game N times 
+            Track the number of wins
+        """ 
+         
         for i in range(N):
             
             self.reset_game()
-            choice_of_player = self.pick_a_random_number(0,2)
-            choice_of_host = None
-            
-            i = (choice_of_player - 1) % self.num_of_doors
-            j = (choice_of_player + 1) % self.num_of_doors
-            
-            if doors[choice_of_player] != "Car":
-                choice_of_host = i if doors[j] == "Car" else j
-            else:
-                choice_of_host = i if self.pick_a_random_number(0,1) <= 0.5 else j
-                
-            new_choice_of_player = None
-
-            new_choice_of_player = j if choice_of_host == i else i 
-
-            if doors[new_choice_of_player] == "Car":
+            self.start_game()
+            if self.player_wins():
                 self.num_of_wins += 1
-           
+            
+                
            
     def print_results(self):
         """ Print win percentage obtained by switching doors
@@ -63,7 +78,7 @@ class MontyHall:
 
 
         
-def to_number(str):
+def to_int(str):
     """ Convert string to integer    
     """
     try:
@@ -74,7 +89,7 @@ def to_number(str):
         sys.exit(0)
 
         
-N = to_number(sys.argv[1])
+N = to_int(sys.argv[1])
 m = MontyHall(N)
 m.start_simulation()
 m.print_results()
